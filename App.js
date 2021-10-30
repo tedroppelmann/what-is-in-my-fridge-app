fb4085a7ea6dca78525bf8992191f4482b22e58b
Tomás Droppelmann <tedroppelmann@uc.cl>
2021-10-30 23:23:46 +0200

    feature auth ready

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDs9yOIS_NCtQyOYZXH66KTQx2be3IgCPo",
+  authDomain: "mobile-app-course-a4cea.firebaseapp.com",
+  projectId: "mobile-app-course-a4cea",
+  storageBucket: "mobile-app-course-a4cea.appspot.com",
+  messagingSenderId: "1052673092228",
+  appId: "1:1052673092228:web:1b2d2e524e538823dd70d1",
+  measurementId: "G-MK3L8MRG8H"
+};
+const app = initializeApp(firebaseConfig);
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing';
+import RegisterScreen from './components/auth/Register';
+import LoginScreen from './components/auth/Login';
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const Stack = createStackNavigator();
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      loaded: false,
+    }
+  }
+
+  componentDidMount() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center'}}>
+          <Text>Loading</Text>
+        </View>
+      )
+    }
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Landing'>
+            <Stack.Screen name ='Landing' component={LandingScreen} options={{headerShown: false}}/>
+            <Stack.Screen name ='Register' component={RegisterScreen}/>
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      )
+    }
+    return (
+      <View style={{ flex: 1, justifyContent: 'center'}}>
+        <Text>User is logged in </Text>
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ components/auth/Register.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { View, Button, TextInput } from 'react-native'
+
+import { getAuth, createUserWithEmailAndPassword }  from 'firebase/auth'
+
+export class Register extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+            name: ''
+        }
+
+        this.onSignUp = this.onSignUp.bind(this)
+    }
+
+    onSignUp() {
+        const { email, password, name } = this.state;
+
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                console.log(result)
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+    }
+
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center'}}>
+                <TextInput
+                    placeholder='name'
+                    onChangeText={(name) => this.setState({ name })}
+                />
+                <TextInput
+                    placeholder='email'
+                    onChangeText={(email) => this.setState({ email })}
+                />
+                <TextInput
+                    placeholder='password'
+                    secureTextEntry={true}
+                    onChangeText={(password) => this.setState({ password })}
+                />
+                <Button
+                    onPress={() => this.onSignUp()}
+                    title='Sign Up'
+                />
+            </View>
+        )
+    }
+}
+
+export default Register
